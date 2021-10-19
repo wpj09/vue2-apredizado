cb89178342968ee4ec876ff9d2ad871ff45bf3cd
wpj09 <91575376+liderveiculos@users.noreply.github.com>
2021-10-19 13:56:28 -0300

    Aprendizado curso Cod3r

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app" :class="{ 'hide-menu': !isMenuVisible }">
+    <Header
+      title="GC - Base de Conhecimento"
+      :hideToggle="false"
+      :hideUserDropdown="false"
+    />
+    <Menu />
+    <Content />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import Header from "@/components/templates/Header.vue";
+import Menu from "@/components/templates/Menu.vue";
+import Content from "@/components/templates/Content.vue";
+import Footer from "@/components/templates/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Menu,
+    Content,
+    Footer,
+  },
+  computed: mapState(["isMenuVisible"]),
+};
+</script>
+
+<style>
+* {
+  font-family: Lato, Verdana, sans-serif;
+  margin: 0;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-snoothing: grayscale;
+
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 50px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
+
+#app.hide-menu {
+  grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer";
+}
+</style>
---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="admin-pages">
+    <PageTitle icon="fa fa-cogs" main="Adminstração do Sistema"
+      sub="Cadastro & Cia" />
+    <div class="admin-pages-tabs">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Artigos" active>
+            <h1>Artigo</h1>
+          </b-tab>
+          <b-tab title="Categorias">
+            <h1>Categorias</h1>
+          </b-tab>
+          <b-tab title="Usuários">
+            <h1>Usuários</h1>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageTitle from "../templates/PageTitle.vue";
+
+export default {
+  name: "AdminPages",
+  components: {
+    PageTitle,
+  },
+};
+</script>
+
+<style>
+</style>
